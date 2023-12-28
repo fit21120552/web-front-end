46facf9298d4c0972334e53a2b2bfc8ec35c14d4
tan <21120552fit@gmail.com>
2023-12-28 17:04:21 +0700

    Admin layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import PublicLayout from "./layouts/PublicLayout";
-import { Home, About } from "./pages";
+import PublicLayout from "./layouts/user/PublicLayout";
+import { Home, About } from "./pages/user/";
+import AdminLayout from "./layouts/admin/AdminLayout";
+import { Dashboard } from "./pages/admin/";
 export default function App() {
   return (
     <div>
@@ -8,6 +10,9 @@ export default function App() {
         <Route path="/" element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="/admin/" element={<Dashboard />} />
         </Route>
       </Routes>
     </div>
