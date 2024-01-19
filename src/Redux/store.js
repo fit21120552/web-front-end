a0888238e7f84634dce5e0094f37f6b87fe1c2d6
tan <21120552fit@gmail.com>
2024-01-19 16:18:07 +0700

    fix conflict redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,19 +1,19 @@
-import { createStore, combineReducers, applyMiddleware } from "redux"
-import thunk from "redux-thunk"
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer } from "./Reducers.js/ProductProducers";
 
 const reducer = combineReducers({
-    productList: productListReducer
+  productList: productListReducer,
 });
 
-const initialState = {}
+const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-    reducer,
-    initialState, 
-    composeWithDevTools(applyMiddleware(...middleware))
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 export default store;
