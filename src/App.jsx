cf21d6dcd24831792d321555473380c65976eb21
tan <21120552fit@gmail.com>
2024-01-22 09:58:35 +0700

    product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.css'
+import "@fortawesome/fontawesome-free/css/all.css";
 import PublicLayout from "./layouts/user/PublicLayout";
 import { Home, About, EditProfile, Cart, Profile } from "./pages/user/";
 import AdminLayout from "./layouts/admin/AdminLayout";
-import { Login, Signup } from "./pages/Authentication/"
+import { Login, Signup } from "./pages/Authentication/";
 import {
   Dashboard,
   Orders,
@@ -13,7 +13,7 @@ import {
   Settings,
   Users,
 } from "./pages/admin/";
-import { ProductDetail } from "./pages/Products";
+import { ProductDetail, Product } from "./pages/Products";
 
 export default function App() {
   return (
@@ -22,11 +22,12 @@ export default function App() {
         <Route path="/" element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/edit-profile" element = {<EditProfile/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/product/:id" element={<ProductDetail/>}/>
-          <Route path="/cart/:id?" element={<Cart/>}/>
+          <Route path="/edit-profile" element={<EditProfile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart/:id?" element={<Cart />} />
         </Route>
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="/admin" exact element={<Dashboard />} />
---
+++ src/pages/Products/Product.jsx
@@ -0,0 +1,192 @@
+import Rating from "../user/Rating";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Loading from "../LoadingError/Loading";
+import Message from "../LoadingError/Message";
+import { Link } from "react-router-dom";
+
+export default function Product() {
+  const loading = false;
+  const error = false;
+
+  const products = [
+    {
+      discountPercentage: "14.87",
+      rating: 4.93,
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+      images: [
+        "https://i.dummyjson.com/data/products/75/1.jpg",
+        "https://i.dummyjson.com/data/products/75/2.jpg",
+        "https://i.dummyjson.com/data/products/75/3.jpg",
+        "https://i.dummyjson.com/data/products/75/thumbnail.jpg",
+      ],
+      _id: "65a9d7b4ad47b243bc49b007",
+      title: "Seven Pocket Women Bag",
+      description:
+        "Seven Pocket Women Bag Handbags Lady Shoulder Crossbody Bag Female Purse Seven Pocket Bag",
+      price: 68,
+      stock: 13,
+      brand: "Steal Frame",
+      category: "womens-bags",
+      thumbnail: "https://i.dummyjson.com/data/products/75/thumbnail.jpg",
+      id: "65a9d7b4ad47b243bc49b007",
+    },
+    {
+      discountPercentage: "14.87",
+      rating: 4.93,
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+      images: [
+        "https://i.dummyjson.com/data/products/75/1.jpg",
+        "https://i.dummyjson.com/data/products/75/2.jpg",
+        "https://i.dummyjson.com/data/products/75/3.jpg",
+        "https://i.dummyjson.com/data/products/75/thumbnail.jpg",
+      ],
+      _id: "65a9d7b4ad47b243bc49b0071",
+      title: "Seven Pocket Women Bag",
+      description:
+        "Seven Pocket Women Bag Handbags Lady Shoulder Crossbody Bag Female Purse Seven Pocket Bag",
+      price: 68,
+      stock: 13,
+      brand: "Steal Frame",
+      category: "womens-bags",
+      thumbnail: "https://i.dummyjson.com/data/products/75/thumbnail.jpg",
+      id: "65a9d7b4ad47b243bc49b0071",
+    },
+    {
+      discountPercentage: "14.87",
+      rating: 4.93,
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+      images: [
+        "https://i.dummyjson.com/data/products/75/1.jpg",
+        "https://i.dummyjson.com/data/products/75/2.jpg",
+        "https://i.dummyjson.com/data/products/75/3.jpg",
+        "https://i.dummyjson.com/data/products/75/thumbnail.jpg",
+      ],
+      _id: "65a9d7b4ad47b243bc49b0072",
+      title: "Seven Pocket Women Bag",
+      description:
+        "Seven Pocket Women Bag Handbags Lady Shoulder Crossbody Bag Female Purse Seven Pocket Bag",
+      price: 68,
+      stock: 13,
+      brand: "Steal Frame",
+      category: "womens-bags",
+      thumbnail: "https://i.dummyjson.com/data/products/75/thumbnail.jpg",
+      id: "65a9d7b4ad47b243bc49b0072",
+    },
+    {
+      discountPercentage: "14.87",
+      rating: 4.93,
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+      images: [
+        "https://i.dummyjson.com/data/products/75/1.jpg",
+        "https://i.dummyjson.com/data/products/75/2.jpg",
+        "https://i.dummyjson.com/data/products/75/3.jpg",
+        "https://i.dummyjson.com/data/products/75/thumbnail.jpg",
+      ],
+      _id: "65a9d7b4ad47b243bc49b0073",
+      title: "Seven Pocket Women Bag",
+      description:
+        "Seven Pocket Women Bag Handbags Lady Shoulder Crossbody Bag Female Purse Seven Pocket Bag",
+      price: 68,
+      stock: 13,
+      brand: "Steal Frame",
+      category: "womens-bags",
+      thumbnail: "https://i.dummyjson.com/data/products/75/thumbnail.jpg",
+      id: "65a9d7b4ad47b243bc49b0073",
+    },
+  ];
+
+  const categories = [
+    {
+      id: 1,
+      name: "May anh",
+      image: "./images/dummy.png",
+    },
+    {
+      id: 2,
+      name: "May tinh",
+      image: "./images/dummy.png",
+    },
+  ];
+
+  return (
+    <div className="container max-w-screen-xl mt-4">
+      <div className="mx-3 px-4 py-3 bg-white mb-[2px]">DANH MỤC</div>
+      <div className=" mx-3 flex mb-4 gap-[2px]">
+        {categories.map((item) => (
+          <div
+            key={item.id}
+            className="bg-white w-28 flex items-center justify-center flex-col py-4"
+          >
+            <div className="w-12 h-12 flex items-center justify-center rounded-full bg-[#f5f5f5]">
+              <img src={item.image} alt="" />
+            </div>
+            <p>{item.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="mx-3 px-4 py-3 bg-white mb-2 text-center text-blue-500 font-semibold border-b-[4px] border-blue-500">
+        GỢI Ý HÔM NAY
+      </div>
+      <div className="section">
+        <Row>
+          <Col className="article">
+            <div className="shopcontainer grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4  gap-3 mx-3">
+              {loading ? (
+                <div className="my-5">
+                  <Loading />
+                </div>
+              ) : error ? (
+                <Message variant="danger">{error}</Message>
+              ) : (
+                <>
+                  {products.map((product) => (
+                    <div
+                      className="shop w-full p-2 rounded-lg border-2 border-solid bg-white"
+                      key={product._id}
+                    >
+                      <div className="border-product">
+                        <Link to={`/product/${product._id}`}>
+                          <div className="shopBack">
+                            <img
+                              src={product.thumbnail}
+                              alt={product.name}
+                              height="100px"
+                              className="bg-[#bbf7d0]"
+                            />
+                          </div>
+                        </Link>
+
+                        <div className="shoptext">
+                          <div>
+                            <Link
+                              to={`/product/${product._id}`}
+                              className="font-serif"
+                            >
+                              {product.title}
+                            </Link>
+                            <Rating
+                              value={product.rating}
+                              text={`${product.ratingsQuantity} reviews`}
+                            ></Rating>
+                            <h3 className="text-2xl font-bold">
+                              ${product.price}
+                            </h3>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </>
+              )}
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
